d6d1590de31ed40b8b9ba0c4dae8332051328bb0
mohamed amdouni <midouamdouni4@gmail.com>
2026-05-13 16:31:22 +0000

    added the layout system

--- headers.h
+++ headers.h
@@ -20,6 +20,7 @@ class SDLinit{
         ~SDLinit();
         void clear();
         void present();
+        void drawtext(int x,int y,const std::string &text);
 };
 SDLinit::SDLinit(int w,int h){
     SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO);
@@ -41,5 +42,54 @@ void SDLinit::clear(void){
 void SDLinit::present(void){
     SDL_RenderPresent(renderer);
 }
+void SDLinit::drawtext(int x,int y,const std::string &text){
+    SDL_Color black = {0,0,0,255};
+    SDL_Surface* surf=TTF_RenderText_Solid(font1,text.c_str(),black);
+    int tw = surf->w;
+    int th = surf->h;
+    SDL_Texture* tex=SDL_CreateTextureFromSurface(renderer,surf);
+    SDL_Rect rect={x,y,tw,th};
+    SDL_RenderCopy(renderer,tex,NULL,&rect);
+    SDL_FreeSurface(surf);
+    SDL_DestroyTexture(tex);
+}
+
+
+class uinter{
+    private:
+        SDLinit& sdl;
+    public:
+        uinter(SDLinit& sdlo);
+        void layout(int* mode);
+        void handle(SDL_Event& event,int &mode);
+};
+uinter::uinter(SDLinit& sdlo):sdl(sdlo){
+}
+void uinter::layout(int* mode){
+    if (*mode==0){ //for this time here is the layout : 0 for adding banks , 1 for view/modify , -1 for map , i all add mroe as the project goes on
+        sdl.drawtext(500,300,"adding banks");
+    }else if (*mode==1){
+        sdl.drawtext(500,300,"viewing bank");
+    }else if (*mode==-1){
+        sdl.drawtext(500,300,"viewing map");
+    }
+}
+void uinter::handle(SDL_Event& event,int &mode){
+    if (event.type==SDL_KEYDOWN){
+        SDL_Keycode key=event.key.keysym.sym;
+        if (key==SDLK_a){
+            mode=0;
+        }else if(key==SDLK_v){
+            mode=1;
+        }else if(key==SDLK_m){
+            mode=-1;
+        }
+
+
+    }
+    
+}
+
+
 
 #endif 
--- main.cpp
+++ main.cpp
@@ -5,21 +5,26 @@
 
 
 int main(void){
+    int mode=-1;
     SDLinit sdl(1280,720);
+    uinter uic(sdl);
     bool running =true;
     SDL_Event event;
     while (running){
         while(SDL_PollEvent(&event)){
-        if (event.type==SDL_QUIT){
-            running=false;
-
-        }
+            if (event.type==SDL_QUIT){
+                running=false;
+                
+            }
+            uic.handle(event, mode);}
         sdl.clear();
+        uic.layout(&mode);
+
         sdl.present();
 
         }
+        sdl.~SDLinit();
     }
-    sdl.~SDLinit();
+    
 
-}
 
