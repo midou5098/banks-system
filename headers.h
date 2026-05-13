4feae023e7c1e03f5806b12820b9cc5a4992b26f
mohamed amdouni <midouamdouni4@gmail.com>
2026-05-13 16:05:05 +0000

    did the basic sdl , widow renders and added the font

--- headers.h
+++ headers.h
@@ -1,5 +1,6 @@
 #ifndef HEADERS_H
-#define HEADERS_H
+
+#include <SDL2/SDL_render.h>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -15,11 +16,30 @@ class SDLinit{
         SDL_Window* window;
         TTF_Font* font1;
     public:
-        SDLinit(void);
-        ~SDLinit(void);
+        SDLinit(int w,int h);
+        ~SDLinit();
+        void clear();
+        void present();
 };
-SDLinit::SDLinit(void){
-    
+SDLinit::SDLinit(int w,int h){
+    SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO);
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
+    window=SDL_CreateWindow("fuckass bank",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,w,h,SDL_WINDOW_SHOWN);
+    renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    font1 =TTF_OpenFont("font.ttf",20);
+}
+SDLinit::~SDLinit(void){
+    IMG_Quit();
+    TTF_Quit();
+    SDL_Quit();
+}
+void SDLinit::clear(void){
+    SDL_SetRenderDrawColor(renderer,140,210,60,255);
+    SDL_RenderClear(renderer);
+}
+void SDLinit::present(void){
+    SDL_RenderPresent(renderer);
 }
 
-
+#endif 
